c3eda1a12002c7e348e8cc50e3083a11df020666
davidgray911 <131274813+davidgray911@users.noreply.github.com>
2023-06-11 23:38:57 +0200

    Update Task6.py

--- Task6.py
+++ Task6.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python
+#wczytywanie do obiektu z pliku .xml i weryfikacja poprawności składni pliku
+import sys
+import xmltodict
+def main():
+        if len(sys.argv) != 2:
+                print("Dozwolony jest tylko jeden argument")
+                return
+        else:
+                pass
+open_file = sys.argv
+a = None
+def open_xml(open_file):
+    try:
+        with open(open_file, 'r') as plik:
+            a = xmltodict.parse(plik.read())
+            return a
+    except FileNotFoundError:
+        print("Taki plik nie istnieje")
+    else:
+        print("Taki plik istnieje")
 
