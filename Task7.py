66b67eaea68f5629206d4853f3b020c7108f42da
davidgray911 <131274813+davidgray911@users.noreply.github.com>
2023-06-11 23:39:33 +0200

    Update Task7.py

--- Task7.py
+++ Task7.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python
+#zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .xml
+import sys
+import xmltodict
+def main():
+        if len(sys.argv) != 2:
+                print("Dozwolony jest tylko jeden argument")
+                return
+        else:
+                pass
+save_file = sys.argv
+a = None
+def save_xml(a, save_file):
+    try:
+        with open(a, 'w') as plik:
+            xml_str = xmltodict.unparse(save_file, pretty=True)
+            plik.write(xml_str)
+        print("Udana konwersja do formatu XML")
+    except Exception:
+        print("Błąd")
 
