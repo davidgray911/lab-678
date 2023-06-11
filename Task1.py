f80f46c0eea9d20d7372de9b61767fa0d53d559e
davidgray911 <131274813+davidgray911@users.noreply.github.com>
2023-06-11 23:35:43 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,35 @@
+#!/usr/bin/python
+#parsowanie argumentów przekazywanych przy uruchomieniu programu
+import sys
+def main():
+        if len(sys.argv) != 3:
+                print("Dozwolone sa tylko dwa argumenty")
+                return
+        else:
+                pass
+
+open_file = sys.argv[1]
+save_file = sys.argv[2]
+
+a = None
+def main():
+        if open_file.endswith('.json'):
+                a = open_json(open_file)
+        elif open_file.endswith('.yaml'):
+                a = open_yaml(open_file)
+        elif open_file.endswith('.xml'):
+                a = open_xml(open_file)
+        else:
+                print("Błędny format pliku.")
+                return
+def main():
+        if save_file.endswith('.json'):
+                save_json(a, save_file)
+        elif save_file.endswith('.yaml'):
+                save_yaml(a, save_file)
+        elif save_file.endswith('.xml'):
+                save_xml(a, save_file)
+        else:
+                print("Błędny format pliku.")
+                return
 
